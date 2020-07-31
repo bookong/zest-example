9a95b7e37b8007692e99b977ced0148c91a91c75
jiangxu <jiang.xu@bestv.com.cn>
2020-07-31 17:43:50 +0800

    init

---
+++ spring-boot/src/test/java/com/github/bookong/example/zest/springboot/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.github.bookong.example.zest.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
+import org.springframework.test.context.BootstrapWith;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
