3f6de5620f575b867c1cfb03342f76096c68db3c
jiangxu <jiang.xu@bestv.com.cn>
2020-08-06 17:01:47 +0800

    init

--- spring-mvc/src/test/java/zest/DemoTest.java
+++ spring-mvc/src/test/java/zest/DemoTest.java
@@ -5,34 +5,42 @@ import com.github.bookong.zest.core.annotation.ZestJdbcConn;
 import com.github.bookong.zest.core.annotation.ZestTest;
 import com.github.bookong.zest.core.testcase.ZestTestParam;
 import com.github.bookong.zest.runner.ZestSpringJUnit4ClassRunner;
+import com.github.bookong.zest.util.ZestSqlHelper;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import javax.activation.DataSource;
+import javax.sql.DataSource;
 import java.sql.Connection;
 
 @RunWith(ZestSpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(locations = { "classpath:applicationContext-test.xml" })
-public class Xx {
+public class DemoTest {
 
     @Autowired
-    protected @ZestDataSource("mysql") DataSource dataSource;
+    protected @ZestDataSource("db1") DataSource dataSource;
 
     @Before
-    public void setUp() throws Exception {
-
+    public void before1() throws Exception {
+        System.out.println("before1()");
     }
 
-    @ZestTest("001.xml")
-    public void testCategoriesCategory(Param params, @ZestJdbcConn("mysql") Connection conn) throws Exception {
-        System.out.println("xx");
+    @Before
+    public void before2() throws Exception {
+        System.out.println("before2()");
     }
 
-    public class Param implements ZestTestParam {
+    @ZestTest("001")
+    public void test1(Param param, @ZestJdbcConn("mysql") Connection conn) throws Exception {
+        ZestSqlHelper.showResultSetContent(conn, "select * form xkcd");
+        System.out.println("test1()");
+    }
 
+    public static class Param implements ZestTestParam {
+
+        private String a;
     }
 }
